5c68a18662d52a330381d373ab096fa9e2d40754
Ewa Orzechowska <orzechowska1989@wp.pl>
2021-08-16 22:31:06 +0200

    Add generate title links

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -39,12 +41,62 @@
   /* add class 'active' to the correct article */
 
   targetArticle.classList.add('active');
+}
+
+ // Generate title links
+
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+    /* remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+      
+    /* find all the articles and save them to variable:  articlesv */
+
+    const articles = document.quertSelectorAll(optArticleSelector)
+    
+    let html = '';
+    
+    for(let article of articles){
+  
+      /* get the article id */
+      const articleId = article.getAttribute('id');
+      console.log(articleId);
+  
+      /* find the title element */
+
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* get the title from the title element */
+  
+      /* create HTML of the link */
+      
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+      /* insert link into html variable */
+
+      html = html + linkHTML;
+    }
+
+    titleList.innerHTML =html;
+
   }
+  
+  generateTitleLinks();
 
   const links = document.querySelectorAll('.titles a');
 
   for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+  link.addEventListener('click', titleClickHandler);
+}
+  
+
+
+
   
   
